3037edba8b8e5ee79226a08d668cc3b040c91e0d
koby-md <104834375+koby-md@users.noreply.github.com>
2025-11-25 17:44:44 +0100

    Create capacitor.config.js

---
+++ capacitor.config.js
@@ -0,0 +1,15 @@
+{
+  "appId": "com.ionic.Kobi",
+  "appName": "Kobi",
+  "webDir": "www",
+  "bundledWebRuntime": false,
+  "server": {
+    "androidScheme": "https",
+    "iosScheme": "https"
+  },
+  "plugins": {
+    "SplashScreen": {
+      "launchShowDuration": 0
+    }
+  }
+}
