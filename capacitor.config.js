ae2156c0aeec8fab87880c0a000f9ab96ab8165b
koby-md <104834375+koby-md@users.noreply.github.com>
2025-11-25 17:58:19 +0100

    Update capacitor.config.js

--- capacitor.config.js
+++ capacitor.config.js
@@ -1,15 +1,17 @@
-{
-  "appId": "com.ionic.botproject",
-  "appName": "BotProject",
-  "webDir": "www",
-  "bundledWebRuntime": false,
-  "server": {
-    "androidScheme": "https",
-    "iosScheme": "https"
-  },
-  "plugins": {
-    "SplashScreen": {
-      "launchShowDuration": 0
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.company.Kobi',
+  appName: 'Kobi',
+  webDir: 'www',
+  plugins: {
+    LiveUpdates: {
+      appId: '042a1261',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
     }
   }
-}
+};
+
+export default config;
