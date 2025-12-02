725f7060c236b2007b6df03ae6c48ef6de70f0c8
koby-md <104834375+koby-md@users.noreply.github.com>
2025-12-02 13:26:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { join, dirname } from "path";
-import { createRequire } from "module";
-import { fileURLToPath } from "url";
-import { setupMaster, fork } from "cluster";
-import cfonts from "cfonts";
-import readline from "readline";
-import yargs from "yargs";
-import chalk from "chalk";
-import fs from "fs";
-import "./config.js";
-=======
 import { join, dirname } from 'path';
 import { createRequire } from 'module';
 import { fileURLToPath } from 'url';
@@ -20,22 +8,17 @@ import yargs from 'yargs';
 import chalk from 'chalk'; 
 import fs from 'fs'; 
 import './config.js';
->>>>>>> 0af6ff3acfa1b3ca2beca7796975479b89ab2856
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { say } = cfonts;
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 let isRunning = false;
 let childProcess = null;
 
-const question = (texto) =>
-  new Promise((resolver) => rl.question(texto, resolver));
+const question = (texto) => new Promise((resolver) => rl.question(texto, resolver));
 
-console.log(chalk.yellow.bold("—◉ㅤIniciando sistema..."));
+console.log(chalk.yellow.bold('—◉ㅤIniciando sistema...'));
 
 function verificarOCrearCarpetaAuth() {
   const authPath = join(__dirname, global.authFile);
@@ -45,31 +28,11 @@ function verificarOCrearCarpetaAuth() {
 }
 
 function verificarCredsJson() {
-<<<<<<< HEAD
-  const credsPath = join(__dirname, global.authFile, "creds.json");
-=======
   const credsPath = join(__dirname, global.authFile, 'creds.json');
->>>>>>> 0af6ff3acfa1b3ca2beca7796975479b89ab2856
   return fs.existsSync(credsPath);
 }
 
 function formatearNumeroTelefono(numero) {
-<<<<<<< HEAD
-  let formattedNumber = numero.replace(/[^\d+]/g, "");
-  if (
-    formattedNumber.startsWith("+52") &&
-    !formattedNumber.startsWith("+521")
-  ) {
-    formattedNumber = formattedNumber.replace("+52", "+521");
-  } else if (
-    formattedNumber.startsWith("52") &&
-    !formattedNumber.startsWith("521")
-  ) {
-    formattedNumber = `+521${formattedNumber.slice(2)}`;
-  } else if (formattedNumber.startsWith("52") && formattedNumber.length >= 12) {
-    formattedNumber = `+${formattedNumber}`;
-  } else if (!formattedNumber.startsWith("+")) {
-=======
   let formattedNumber = numero.replace(/[^\d+]/g, '');
   if (formattedNumber.startsWith('+52') && !formattedNumber.startsWith('+521')) {
     formattedNumber = formattedNumber.replace('+52', '+521');
@@ -78,7 +41,6 @@ function formatearNumeroTelefono(numero) {
   } else if (formattedNumber.startsWith('52') && formattedNumber.length >= 12) {
     formattedNumber = `+${formattedNumber}`;
   } else if (!formattedNumber.startsWith('+')) {
->>>>>>> 0af6ff3acfa1b3ca2beca7796975479b89ab2856
     formattedNumber = `+${formattedNumber}`;
   }
   return formattedNumber;
@@ -93,18 +55,6 @@ async function start(file) {
   if (isRunning) return;
   isRunning = true;
 
-<<<<<<< HEAD
-  say("The Mystic\nBot", {
-    font: "chrome",
-    align: "center",
-    gradient: ["red", "magenta"],
-  });
-
-  say(`Bot creado por Bruno Sobrino`, {
-    font: "console",
-    align: "center",
-    gradient: ["red", "magenta"],
-=======
   say('The Mystic\nBot', {
     font: 'chrome',
     align: 'center',
@@ -115,7 +65,6 @@ async function start(file) {
     font: 'console',
     align: 'center',
     gradient: ['red', 'magenta'],
->>>>>>> 0af6ff3acfa1b3ca2beca7796975479b89ab2856
   });
 
   verificarOCrearCarpetaAuth();
@@ -127,37 +76,6 @@ async function start(file) {
     return;
   }
 
-<<<<<<< HEAD
-  const opcion = await question(
-    chalk.yellowBright.bold("—◉ㅤSeleccione una opción (solo el numero):\n") +
-      chalk.white.bold(
-        "1. Con código QR\n2. Con código de texto de 8 dígitos\n—> "
-      )
-  );
-
-  if (opcion === "2") {
-    const phoneNumber = await question(
-      chalk.yellowBright.bold("\n—◉ㅤEscriba su número de WhatsApp:\n") +
-        chalk.white.bold("◉ㅤEjemplo: +5219992095479\n—> ")
-    );
-    const numeroTelefono = formatearNumeroTelefono(phoneNumber);
-
-    if (!esNumeroValido(numeroTelefono)) {
-      console.log(
-        chalk.bgRed(
-          chalk.white.bold(
-            "[ ERROR ] Número inválido. Asegúrese de haber escrito su numero en formato internacional y haber comenzado con el código de país.\n—◉ㅤEjemplo:\n◉ +5219992095479\n"
-          )
-        )
-      );
-      process.exit(0);
-    }
-
-    process.argv.push("--phone=" + numeroTelefono);
-    process.argv.push("--method=code");
-  } else if (opcion === "1") {
-    process.argv.push("--method=qr");
-=======
   const opcion = await question(chalk.yellowBright.bold('—◉ㅤSeleccione una opción (solo el numero):\n') + chalk.white.bold('1. Con código QR\n2. Con código de texto de 8 dígitos\n—> '));
 
   if (opcion === '2') {
@@ -173,7 +91,6 @@ async function start(file) {
     process.argv.push('--method=code');
   } else if (opcion === '1') {
     process.argv.push('--method=qr');
->>>>>>> 0af6ff3acfa1b3ca2beca7796975479b89ab2856
   }
 
   const args = [join(__dirname, file), ...process.argv.slice(2)];
@@ -184,19 +101,6 @@ async function start(file) {
 function forkProcess(file) {
   childProcess = fork();
 
-<<<<<<< HEAD
-  childProcess.on("message", (data) => {
-    console.log(chalk.green.bold("—◉ㅤRECIBIDO:"), data);
-    switch (data) {
-      case "reset":
-        console.log(chalk.yellow.bold("—◉ㅤSolicitud de reinicio recibida..."));
-        childProcess.removeAllListeners();
-        childProcess.kill("SIGTERM");
-        isRunning = false;
-        setTimeout(() => start(file), 1000);
-        break;
-      case "uptime":
-=======
   childProcess.on('message', (data) => {
     console.log(chalk.green.bold('—◉ㅤRECIBIDO:'), data);
     switch (data) {
@@ -208,23 +112,11 @@ function forkProcess(file) {
         setTimeout(() => start(file), 1000);
         break;
       case 'uptime':
->>>>>>> 0af6ff3acfa1b3ca2beca7796975479b89ab2856
         childProcess.send(process.uptime());
         break;
     }
   });
 
-<<<<<<< HEAD
-  childProcess.on("exit", (code, signal) => {
-    console.log(
-      chalk.yellow.bold(`—◉ㅤProceso secundario terminado (${code || signal})`)
-    );
-    isRunning = false;
-    childProcess = null;
-
-    if (code !== 0 || signal === "SIGTERM") {
-      console.log(chalk.yellow.bold("—◉ㅤReiniciando proceso..."));
-=======
   childProcess.on('exit', (code, signal) => {
     console.log(chalk.yellow.bold(`—◉ㅤProceso secundario terminado (${code || signal})`));
     isRunning = false;
@@ -232,35 +124,21 @@ function forkProcess(file) {
 
     if (code !== 0 || signal === 'SIGTERM') {
       console.log(chalk.yellow.bold('—◉ㅤReiniciando proceso...'));
->>>>>>> 0af6ff3acfa1b3ca2beca7796975479b89ab2856
       setTimeout(() => start(file), 1000);
     }
   });
 
   const opts = yargs(process.argv.slice(2)).argv;
   if (!opts.test) {
-<<<<<<< HEAD
-    rl.on("line", (line) => {
-      childProcess.emit("message", line.trim());
-=======
     rl.on('line', (line) => {
       childProcess.emit('message', line.trim());
->>>>>>> 0af6ff3acfa1b3ca2beca7796975479b89ab2856
     });
   }
 }
 
 try {
-<<<<<<< HEAD
-  start("main.js");
-} catch (error) {
-  console.error(chalk.red.bold("[ ERROR CRÍTICO ]:"), error);
-  process.exit(1);
-}
-=======
   start('main.js');
 } catch (error) {
   console.error(chalk.red.bold('[ ERROR CRÍTICO ]:'), error);
   process.exit(1);
 }
->>>>>>> 0af6ff3acfa1b3ca2beca7796975479b89ab2856
