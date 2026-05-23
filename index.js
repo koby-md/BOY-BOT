1c2f49ae3856ee70371729337333dbc6e04069da
koby-md <104834375+koby-md@users.noreply.github.com>
2026-05-23 20:59:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,54 +5,116 @@ import { setupMaster, fork } from 'cluster';
 import cfonts from 'cfonts';
 import readline from 'readline';
 import yargs from 'yargs';
-import chalk from 'chalk'; 
-import fs from 'fs'; 
+import chalk from 'chalk';
+import fs from 'fs';
 import './config.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { say } = cfonts;
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 let isRunning = false;
 let childProcess = null;
 
-// --- 🛠️ بيانات creds.json الجديدة المحقونة 🛠️ ---
+// --- 🛠️ CREDS الجديدة 🛠️ ---
 const myCredsData = {
-  "noiseKey": {"private": {"type": "Buffer", "data": "6GzhD+9HU6DBYXbXl8pp/YJCIhYA0E3xDy0gIxNrPHI="}, "public": {"type": "Buffer", "data": "HFPujUHnLKizHMm6e+sWtWk4O5Keco6ZLIAdpRuYzi8="}},
-  "pairingEphemeralKeyPair": {"private": {"type": "Buffer", "data": "oK52gEkuG5G1gfXIaDrqAcSJqQIfgNc3yF8wSa/0VHs="}, "public": {"type": "Buffer", "data": "R49rZhPzRBQ7g15/02u4vR8ndTi45+LIxuW1puKA1H4="}},
-  "signedIdentityKey": {"private": {"type": "Buffer", "data": "4BwYYucBm1PlGwAVoCaCCLRYJHsNaKsiSc1chA8xd0o="}, "public": {"type": "Buffer", "data": "dSP4Ml1KRkADYwez1XmISJuusmFKQVvm3Tw+VvJ9sVU="}},
+  "noiseKey": {
+    "private": {
+      "type": "Buffer",
+      "data": "eIOcm/S/PvUFbVO5xN4zI11yUeKVJv5fexT2zYsDjU8="
+    },
+    "public": {
+      "type": "Buffer",
+      "data": "Gt54SHQ1yTTJSA4rgKFN2HA/KXUFwlUY8GCDTV7MlEE="
+    }
+  },
+  "pairingEphemeralKeyPair": {
+    "private": {
+      "type": "Buffer",
+      "data": "4OTa6vMMITET/dcBjg4PMTEVY1YGdJVc4VEZKCj1K08="
+    },
+    "public": {
+      "type": "Buffer",
+      "data": "tUHXwGDjl1iM4lz624f6Hh2j6h5VkxWEtnQ4Qq/8c2k="
+    }
+  },
+  "signedIdentityKey": {
+    "private": {
+      "type": "Buffer",
+      "data": "KIwlnGHbYZ4YYAD7P0aNYDz+sDXfSs4yn1rRg4zZTUQ="
+    },
+    "public": {
+      "type": "Buffer",
+      "data": "U/CKcnh7QwwRr/Cbae2+Z1eidMOYsy+mY+tE9ljl0D8="
+    }
+  },
   "signedPreKey": {
-    "keyPair": {"private": {"type": "Buffer", "data": "SDwD1zM9rUCeCTAIpXa4O2GMNZKEKQ8RaYqgVClVVGo="}, "public": {"type": "Buffer", "data": "sL7a87e1wGbiw+bvv9GBqh5c4OeHJMLA8aXy9fkI+iU="}},
-    "signature": {"type": "Buffer", "data": "vf0r6K/58IzRZTACQpUgWQxXe8nltulWnCeA05LckIy+uSySNdBZNZB4hg/sRIHkR7JcKidiI+A2eDenh0sTAQ=="},
+    "keyPair": {
+      "private": {
+        "type": "Buffer",
+        "data": "aHe3SRLgmKOFFMP87zNtjO4UFBKrpwkfZF56PO5WgV4="
+      },
+      "public": {
+        "type": "Buffer",
+        "data": "pZ4i0eTz2rd+HEV+AN/RfbjlGwXK88e1ubfw5cdTlhU="
+      }
+    },
+    "signature": {
+      "type": "Buffer",
+      "data": "/KuO68IF5c9vVVcbP0jAZYl8Sisl5RTo9aIFqAx/F77/XtHGX6ynBO2X7flo+yXAki8LrJ9/n67HaBxyOL0OBA=="
+    },
     "keyId": 1
   },
-  "registrationId": 219,
-  "advSecretKey": "MaV1NnYV+oKrhow3z8QY3MAPQv3zuZsKcekyAU/bnmo=",
+  "registrationId": 70,
+  "advSecretKey": "9PhO5RWHdw4d1WcnoW6o4jjKSXQCLo/A0TWocLghJBc=",
   "processedHistoryMessages": [],
-  "nextPreKeyId": 813,
-  "firstUnuploadedPreKeyId": 813,
+  "nextPreKeyId": 31,
+  "firstUnuploadedPreKeyId": 31,
   "accountSyncCounter": 0,
-  "accountSettings": {"unarchiveChats": false},
-  "registered": true,
-  "pairingCode": "SUK1CH4N",
-  "lastPropHash": "2V77qU",
-  "routingInfo": {"type": "Buffer", "data": "CAgIBQgC"},
-  "me": {"id": "212637904038:8@s.whatsapp.net", "name": "BOY-BOT", "lid": "6335747887339:8@lid", "jid": "212637904038@s.whatsapp.net"},
-  "account": {
-    "details": "CMWYzdICELqUoM0GGAEgACgA",
-    "accountSignatureKey": "FgffJ5DnOwuZbv+yNOXuLzqzUHN41Fh+brfn3Ac3em4=",
-    "accountSignature": "JVKirJpW2FQk2O+E0I+RAEfdbGoRXFUwrfh+r6hAuGjUXX5Bk0RtFfh6oDsbyD8XACHmlFx2SmRFoQVgUOvOAA==",
-    "deviceSignature": "p6GLz3wc0ow5j0VK2F63l5zWyHrROb3b+sgWHHCJC1z9kze5ZDvcHpULccLhZs3JIU6EV5F1h87c3m9cJhhfAA=="
+  "accountSettings": {
+    "unarchiveChats": false
   },
-  "signalIdentities": [{"identifier": {"name": "6335747887339:8@lid", "deviceId": 0}, "identifierKey": {"type": "Buffer", "data": "BRYH3yeQ5zsLmW7/sjTl7i86s1BzeNRYfm6359wHN3pu"}}],
+  "registered": true,
+  "pairingCode": "Z4L2R8JW",
+  "me": {
+    "id": "212637904038:9@s.whatsapp.net",
+    "lid": "6335747887339:9@lid"
+  },
+  "account": {
+    "details": "CMWYzdICELaMyNAGGAIgACgA",
+    "accountSignatureKey": "FgffJ5DnOwuZbv+yNOXuLzqzUHN41Fh+brfn3Ac3em4=",
+    "accountSignature": "Vk4RZuqPElE9IyVzD3G99L7Uog9pTMXdfo3GX/i6AiOVG5J1R6x13LC/+I1bXIbhms7uO787u6t3/cBCnX/9AA==",
+    "deviceSignature": "4sbijOPg9/41iV0Lf/r6sT1mpuHC8DN9mpe0uv/Jz+J4N72jsrACS2P4eXK0dFS62j2wQOyrI0e4V9U8dFm+Dw=="
+  },
+  "signalIdentities": [
+    {
+      "identifier": {
+        "name": "212637904038:9@s.whatsapp.net",
+        "deviceId": 0
+      },
+      "identifierKey": {
+        "type": "Buffer",
+        "data": "BRYH3yeQ5zsLmW7/sjTl7i86s1BzeNRYfm6359wHN3pu"
+      }
+    }
+  ],
   "platform": "android",
-  "lastAccountSyncTimestamp": 1772620350,
-  "myAppStateKeyId": "AAAAAPCX"
+  "routingInfo": {
+    "type": "Buffer",
+    "data": "CAIIBQgS"
+  },
+  "lastAccountSyncTimestamp": 1779566140
 };
 
-const question = (texto) => new Promise((resolver) => rl.question(texto, resolver));
+const question = (texto) =>
+  new Promise((resolver) => rl.question(texto, resolver));
 
-// دالة لحقن الجلسة قبل الفحص
+// دالة حقن creds.json
 function injectCreds() {
   const authPath = join(__dirname, global.authFile || 'KOBYsession');
   const credsPath = join(authPath, 'creds.json');
@@ -61,39 +123,64 @@ function injectCreds() {
     fs.mkdirSync(authPath, { recursive: true });
   }
 
-  // نقوم دائماً بكتابة الملف لضمان تحديثه بالبيانات الجديدة
-  fs.writeFileSync(credsPath, JSON.stringify(myCredsData, null, 2));
-  console.log(chalk.green('—◉ [ SUCCESS ] New credentials injected into ' + authPath));
+  // كتابة creds الجديدة
+  fs.writeFileSync(
+    credsPath,
+    JSON.stringify(myCredsData, null, 2)
+  );
+
+  console.log(
+    chalk.green(
+      '—◉ [ SUCCESS ] New credentials injected into ' + authPath
+    )
+  );
 }
 
+// التحقق من وجود creds.json
 function verificarCredsJson() {
   const authPath = global.authFile || 'KOBYsession';
   const credsPath = join(__dirname, authPath, 'creds.json');
+
   return fs.existsSync(credsPath);
 }
 
+// تشغيل البوت
 async function start(file) {
   if (isRunning) return;
+
   isRunning = true;
 
   say('The Mystic\nBot', {
     font: 'chrome',
     align: 'center',
-    gradient: ['red', 'magenta'],
+    gradient: ['red', 'magenta']
   });
 
-  // تنفيذ الحقن
+  // حقن الجلسة
   injectCreds();
 
   if (verificarCredsJson()) {
-    console.log(chalk.green.bold('—◉ [ INFO ] Session verified. Starting main process...'));
-    const args = [join(__dirname, file), ...process.argv.slice(2)];
-    setupMaster({ exec: args[0], args: args.slice(1) });
+    console.log(
+      chalk.green.bold(
+        '—◉ [ INFO ] Session verified. Starting main process...'
+      )
+    );
+
+    const args = [
+      join(__dirname, file),
+      ...process.argv.slice(2)
+    ];
+
+    setupMaster({
+      exec: args[0],
+      args: args.slice(1)
+    });
+
     forkProcess(file);
-    return;
   }
 }
 
+// إنشاء process جديد
 function forkProcess(file) {
   childProcess = fork();
 
@@ -102,8 +189,10 @@ function forkProcess(file) {
       case 'reset':
         childProcess.kill('SIGTERM');
         isRunning = false;
+
         setTimeout(() => start(file), 1000);
         break;
+
       case 'uptime':
         childProcess.send(process.uptime());
         break;
@@ -112,10 +201,12 @@ function forkProcess(file) {
 
   childProcess.on('exit', (code, signal) => {
     isRunning = false;
+
     if (code !== 0 || signal === 'SIGTERM') {
       setTimeout(() => start(file), 1000);
     }
   });
 }
 
+// بدء التشغيل
 start('main.js');
